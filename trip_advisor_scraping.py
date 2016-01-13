dd376603c5eae98a4b667a877e0af83c804a8912
Giorgos Margaritis <gmargari@cs.uoi.gr>
2016-01-14 00:51:21 +0200

    Insignificant commit

--- trip_advisor_scraping.py
+++ trip_advisor_scraping.py
@@ -7,17 +7,14 @@ import json
 import sys
 import re
 
-
-# Attractions
 urls = [
-"http://www.tripadvisor.com/Attractions-g189473-Activities-Thessaloniki_Thessaloniki_Region_Central_Macedonia.html",
-"http://www.tripadvisor.com/Attractions-g189473-Activities-oa30-Thessaloniki_Thessaloniki_Region_Central_Macedonia.html",
-"http://www.tripadvisor.com/Attractions-g189473-Activities-oa60-Thessaloniki_Thessaloniki_Region_Central_Macedonia.html",
-"http://www.tripadvisor.com/Attractions-g189473-Activities-oa90-Thessaloniki_Thessaloniki_Region_Central_Macedonia.html",
+    "http://www.tripadvisor.com/Attractions-g189473-Activities-Thessaloniki_Thessaloniki_Region_Central_Macedonia.html",
+    "http://www.tripadvisor.com/Attractions-g189473-Activities-oa30-Thessaloniki_Thessaloniki_Region_Central_Macedonia.html",
+    "http://www.tripadvisor.com/Attractions-g189473-Activities-oa60-Thessaloniki_Thessaloniki_Region_Central_Macedonia.html",
+    "http://www.tripadvisor.com/Attractions-g189473-Activities-oa90-Thessaloniki_Thessaloniki_Region_Central_Macedonia.html",
 ]
 userId = "tripadvisor_poiscrapper"
 
-url = "http://www.tripadvisor.com/Attractions-g189473-Activities-Thessaloniki_Thessaloniki_Region_Central_Macedonia.html#ATTRACTION_LIST"
 for url in urls:
     hostname = urlparse(url).hostname
     soup = BeautifulSoup(urlopen(url).read(), "lxml")
@@ -28,15 +25,13 @@ for url in urls:
         tags    = [ tag.text for tag in elem.select("div.p13n_reasoning_v2")[0].select("span") ]
         fullurl = "http://" + hostname + "/" + urlsuf
 
-
         # Get javascript
         soup = BeautifulSoup(urlopen(fullurl).read(), "lxml")
         # Find element that contains the "CurrentCenter.png" and lat,lon
         for script in soup.find_all('script'):
+            # Match text between "CurrentCenter.png|" and "&language". Will get something like "40.6383,22.94802"
             latlon = re.findall("(?<=CurrentCenter.png\|)(.*)(?=\&language)", script.text)
             if (latlon):
-        # Match text between "CurrentCenter.png|" and "&language". Will get something like "40.6383,22.94802"
-        #latlon = re.findall("(?<=CurrentCenter.png\|)(.*)(?=\&language)", script_69)
                 latlon_arr = latlon[0].split(",")
                 latitude = latlon_arr[0]
                 longitude = latlon_arr[1]
@@ -50,6 +45,7 @@ for url in urls:
             "longitude" : longitude,
             "userId" : userId,
         }
+
         print json.dumps(poi)
 
 
